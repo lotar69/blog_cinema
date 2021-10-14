e76f62988ab1f3bd07536a5b7e6eb864d93bccc9
lotar69 <lotardy@gmail.com>
2021-10-14 19:06:55 +0200

    first real commit

---
+++ src/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from posts.views import BlogHome, BlogPostCreate
+
+app_name = "posts"
+
+urlpatterns = [
+    path("", BlogHome.as_view(), name="home"),
+    path("create/", BlogPostCreate.as_view(), name="create"),
+]
